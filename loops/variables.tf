77db9d0b0f031f2654b075a101ba86f3a08afce9
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-06-10 22:04:46 +0530

    terraform r53 started

---
+++ loops/variables.tf
@@ -0,0 +1,62 @@
+
+variable "ami_id" {
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "AMI ID of joindevops RHEL9"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ec2_tags" {
+    type = map(string)
+    default = {
+        Name = "HelloWorld"
+        Purpose = "variables-demo"
+    }
+}
+
+variable "sg_name" {
+    #default = "vars-file-allow-all"
+}
+
+variable "sg_description" {
+    default = "allowing all ports from internet"
+}
+
+variable "from_port" {
+    default = 0
+}
+
+variable "to_port" {
+    type = number
+    default = 0
+}
+
+variable "cidr_blocks" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+    default = {
+        Name = "allow-all"
+    }
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "instances" {
+  default = ["mongodb", "redis", "mysql", "rabbitmq"]
+}
+
+variable "zone_id" {
+  default = "Z040983115KD520HXV305"
+}
+
+variable "domain_name" {
+  default = "hemalatha.site"
+}
